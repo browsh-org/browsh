8d958b0256cffeca0ad3620793542cb8d76e9ff3
Thomas Buckley-Houston <tom@tombh.co.uk>
2018-06-05 15:01:52 +0800

    Readded the browser resize height magic number
    This fixes a bug where the bottom of web pages wouldn't get parsed.
    
    And also another bug where mouse clicks at the bottom of a page
    wouldn't render.

--- webext/src/background/dimensions.js
+++ webext/src/background/dimensions.js
@@ -8,6 +8,14 @@ export default class extends utils.mixins(CommonMixin) {
     super();
     this.tty = {};
     this.char = {};
+    // I *think* this extra height is needed because the browser window height is not the same
+    // as the actual viewport height. But then if that is the case, then we'll also have a
+    // similar issue with the scroll bars.
+    // TODO: Also if this hypothesis is correct, it needs to be applied as an original browser-
+    // relative pixel unit, not as a TTY unit. If you look on Google Maps I think you can
+    // actually see a little bit of white at the bottom perhaps from where the screen capture
+    // goes over the bottom of the viewport.
+    this._window_ui_magic_number = 3;
     // The Browsh HTTP Server service doesn't load a TTY, so we need to supply the size.
     // Strictly it shouldn't even be needed if the code was completely refactored. Although
     // it should be worth taking into consideration how the size of the TTY and therefore the
@@ -44,7 +52,7 @@ export default class extends utils.mixins(CommonMixin) {
     // Leave room for tabs and URL bar
     const tty_dom_height = this.tty.height - 2;
     const window_height = parseInt(Math.round(
-      tty_dom_height * this.char.height
+      (tty_dom_height + this._window_ui_magic_number) * this.char.height
     ));
     const current_window = browser.windows.getCurrent();
     current_window.then(
