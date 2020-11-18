bd9047c19443426e96a317caf29e9b0285e2b202
Yash Singh <53054099+Yash-Singh1@users.noreply.github.com>
2020-11-18 09:25:31 -0800

    Added in Ctrl+U option for view-source

--- interfacer/src/browsh/tty.go
+++ interfacer/src/browsh/tty.go
@@ -65,6 +65,10 @@ func handleUserKeyPress(ev *tcell.EventKey) {
 		urlBarFocusToggle()
 	case tcell.KeyCtrlT:
 		createNewEmptyTab()
+	case tcell.KeyCtrlU:
+		if isNewEmptyTabActive() {
+			sendMessageToWebExtension("/new_tab,view-source:" + CurrentTab.URI)
+		}
 	case tcell.KeyCtrlW:
 		removeTab(CurrentTab.ID)
 	case tcell.KeyBackspace, tcell.KeyBackspace2:
